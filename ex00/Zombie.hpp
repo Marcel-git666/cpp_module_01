adb49a0329ec2fc15c70d8e41b47e9b431503b21
Marcel Mravec <marcel.mravec@evolwings.net>
2025-10-20 14:05:00 +0200

    ex00 done?

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -13,7 +13,7 @@ public:
   void announce(void);
 };
 
-#endif
-
 Zombie *newZombie(std::string name);
-void randomChump(std::string name);
+void randomChump(const std::string &);
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+int main(void) {
+  randomChump("Stack Zombak");
+  Zombie *heapZombie = newZombie("Heap Zombak");
+  heapZombie->announce();
+  delete heapZombie;
+  return 0;
+}
