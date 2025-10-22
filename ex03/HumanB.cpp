ff4cceacab85b8a73ded6a71884123ddf65abe02
Marcel Mravec <marcel.mravec@evolwings.net>
2025-10-22 10:49:39 +0200

    improve makefile, fix spaces.

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -7,9 +7,10 @@ HumanB::HumanB(const std::string &name) : name(name), weapon(NULL) {}
 
 void HumanB::attack(void) {
     if (weapon) {
-        std::cout << name << "attacks with their" << weapon->getType() << '\n';
+        std::cout << name << " attacks with their " << weapon->getType()
+                  << '\n';
     } else {
-        std::cout << name << "attacks unarmed" << '\n';
+        std::cout << name << " attacks unarmed" << '\n';
     }
 }
 
